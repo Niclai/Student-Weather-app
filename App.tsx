59e4f78b2c005df77ae31000ec219e0eb1729c3d
IvanSeagull <47693465+IvanSeagull@users.noreply.github.com>
2023-02-22 10:08:00 +0000

    Setup project

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <NavigationContainer>
+        <Text>Setup project</Text>
+        <StatusBar style="auto" />
+      </NavigationContainer>
     </View>
   );
 }
