a3b9a08abc94a5202280928de1e4a6606cdfce62
Kishan <kishankumaran03.gmail.com>
2023-03-04 19:36:17 +0000

    updated docstrings

--- client/src/types/userPreferences.ts
+++ client/src/types/userPreferences.ts
@@ -1,30 +1,30 @@
 export interface UserPreferences {
   hayFever: boolean;
-  timesPerWeek: number;
   /**
    * how many times a week to be notified (1-7)
    */
-  timeBeforeNotif: number;
+  timesPerWeek: number;
   /**
    * how long before study space to be notified (hours)
    */
-  studyDuration: number;
+  timeBeforeNotif: number;
   /**
    * units: hours
+   */
+  studyDuration: number;
+  /**
+   * units: degrees celcius
    */
   preferredMinTemp: number;
   preferredMaxTemp: number;
   /**
-   * units: degrees celcius
-   */
-  maxWindSpeed: number;
-  /**
    * default=5 (km/h)
    */
-  pollenLevels: number;
+  maxWindSpeed: number;
   /**
    * units: pollen count (grains per volume of air)
    * only needed if hayFever=True
    */
+  pollenLevels: number;
   }
   
