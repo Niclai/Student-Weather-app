fec238e2ce2ac60c2074db3fad0f184cb5ee226b
Dawid Lachowicz <domdaw7@gmail.com>
2023-03-11 13:25:07 +0000

    Add getCurrentLocation to fetch name and coords from GPS

--- client/src/api/location.ts
+++ client/src/api/location.ts
@@ -1,14 +1,35 @@
-import * as Location from "expo-location";
-import { Coordinates } from "../types/location";
+import * as GPSLocation from "expo-location";
+import { Coordinates, Location } from "../types/location";
 
 async function getCurrentCoordinates(): Promise<Coordinates> {
-  const { status } = await Location.requestForegroundPermissionsAsync();
+  const { status } = await GPSLocation.requestForegroundPermissionsAsync();
   if (status !== "granted") {
     throw new Error("Permission to access location was denied");
   }
 
-  const location = await Location.getCurrentPositionAsync({});
+  const location = await GPSLocation.getCurrentPositionAsync({});
   return { long: location.coords.longitude, lat: location.coords.latitude };
 }
 
-export { getCurrentCoordinates };
+interface GoogleMapsReverseGeocodingResponse {
+  results: {
+    formatted_address: string;
+  }[];
+}
+
+async function getLocationName(coords: Coordinates): Promise<string> {
+  const response = await fetch(
+    `${baseUrl}/maps/api/geocode/json?latlng=${coords.lat},${coords.long}&result_type=political`
+  );
+  const json: GoogleMapsReverseGeocodingResponse = await response.json();
+  return json.results[0].formatted_address;
+}
+
+async function getCurrentLocation(): Promise<Location> {
+  const coordinates = await getCurrentCoordinates();
+  const locationName = await getLocationName(coordinates);
+
+  return { name: locationName, coords: coordinates };
+}
+
+export { getCurrentLocation };
