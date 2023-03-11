6d9884723c2bd4073826edd718b0566a5c7ad12e
Dawid Lachowicz <domdaw7@gmail.com>
2023-03-11 14:03:37 +0000

    Add GoogleMapsGeocodingResponse interface

--- client/src/api/location.ts
+++ client/src/api/location.ts
@@ -1,12 +1,24 @@
 import { baseUrl } from "../env/variables";
 import { Coordinates } from "../types/location";
 
+interface GoogleMapsGeocodingResponse {
+  results: {
+    geometry: {
+      location: {
+        lat: number;
+        lng: number;
+      };
+    };
+  }[];
+}
+
 async function getCoordinates(placeId: string): Promise<Coordinates> {
   const response = await fetch(
     `${baseUrl}/maps/api/geocode/json?place_id=${placeId}`
   );
-  const json = await response.json();
-  return json.results[0].geometry.location;
+  const json: GoogleMapsGeocodingResponse = await response.json();
+  const { lat, lng } = json.results[0].geometry.location;
+  return { lat, long: lng };
 }
 
 export { getCoordinates };
