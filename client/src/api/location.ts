cf498247228f39cf8a81cf8c5eb89d6b3ad24f97
Dawid Lachowicz <domdaw7@gmail.com>
2023-03-11 13:11:45 +0000

    Use const instead of let

--- client/src/api/location.ts
+++ client/src/api/location.ts
@@ -2,12 +2,12 @@ import * as Location from "expo-location";
 import { Coordinates } from "../types/location";
 
 async function getCurrentCoordinates(): Promise<Coordinates> {
-  let { status } = await Location.requestForegroundPermissionsAsync();
+  const { status } = await Location.requestForegroundPermissionsAsync();
   if (status !== "granted") {
     throw new Error("Permission to access location was denied");
   }
 
-  let location = await Location.getCurrentPositionAsync({});
+  const location = await Location.getCurrentPositionAsync({});
   return { long: location.coords.longitude, lat: location.coords.latitude };
 }
 
