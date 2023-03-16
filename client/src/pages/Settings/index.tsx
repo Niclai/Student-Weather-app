5c64c048c183f300488b2e1fe9b227cd47fdcdb8
Nicholas Lai <n.lai8@outlook.com>
2023-03-16 18:19:52 +0000

    resolve merging conflicts

--- client/src/pages/Settings/index.tsx
+++ client/src/pages/Settings/index.tsx
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
-import UserPreferenceForm from '../../components/userPreferenceForm'; 
-=======
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
->>>>>>> 2e078038d79355c365ba901ac5d8692e27cb620d
 
 const Settings = () => {
   return (
