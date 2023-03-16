fe3545e278135ac745ea6c2f20fd25ef057e72ca
Nicholas Lai <n.lai8@outlook.com>
2023-03-16 18:10:13 +0000

    Created base component ShowForecastCurrentDay.tsx

---
+++ client/src/components/Forecasts/ShowForecastCurrentDay.tsx
@@ -0,0 +1,15 @@
+import { View } from "react-native";
+import { getDayForecast } from "../../api/forecast";
+
+const ShowCurrentDayForecast = () => {
+
+
+
+
+
+
+    
+    return (
+        <View></View>
+    );
+}
