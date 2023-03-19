bf210ffd7e3bcb04daf6d74d4e93d91dfd9e4058
Dawid Lachowicz <domdaw7@gmail.com>
2023-03-19 21:36:27 +0000

    Document Home/index.tsx

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,11 +1,18 @@
 import React, { useContext } from "react";
 import { Text } from "react-native";
+
+// Components
 import FirstTimeSetup from "../../components/Home/FirstTimeSetup";
 import MainScreen from "../../components/Home/MainScreen";
 
 // Context
 import { UserPreferencesContext } from "../../providers/UserPreferences";
 
+/**
+ * Home page component. The first time the user launches the application,
+ * it renders the FirstTimeSetup component. Once the initial setup the complete
+ * by the user, the MainScreen is rendered.
+ */
 const Home = () => {
   const { userPreferences } = useContext(UserPreferencesContext);
 
